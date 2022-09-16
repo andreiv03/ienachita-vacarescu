ea76152cb3d56ba9e823d4ac57fd752d5ba2110b
Andrei Voicu <andrei.voicu110@gmail.com>
2022-09-16 22:42:18 +0300

    Update label.tsx

--- components/label.tsx
+++ components/label.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/components/label.module.scss";
 const Label: React.FC = () => {
   return (
     <div className={styles.label}>
-      <h6>The school&apos;s core values became dignity, professionalism, renewal, creativity and reinterpretation.</h6>
+      <h6>Education - Performance - Multiculturalism</h6>
     </div>
   );
 }
